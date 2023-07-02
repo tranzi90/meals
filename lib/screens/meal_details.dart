9ad34f9e1891878c2f431a2ceb23d2b00eede785
denis_levchenko <tranzi90@gmail.com>
2023-07-02 23:49:31 +0300

    navigate to meal details screen

---
+++ lib/screens/meal_details.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+class MealDetailsScreen extends StatelessWidget {
+  const MealDetailsScreen({
+    super.key,
+    required this.meal,
+  });
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: Image.network(meal.imageUrl),
+    );
+  }
+}
